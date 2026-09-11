e7b6291d25eb55d46c09470d8d4e19a90ccbde9f
Jax <jaxxonhalsall@gmail.com>
2026-09-11 12:15:02 +0930

    Added returnToMenuButton

--- UniversalCalculator/UnitsConverter.xaml.cs
+++ UniversalCalculator/UnitsConverter.xaml.cs
@@ -27,5 +27,10 @@ namespace Calculator
 		{
 			InitializeComponent();
 		}
+
+		private void returnToMenuButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MainMenu));
+		}
 	}
 }
