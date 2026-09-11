db0f71353748cb0d9a26479b03972f4bb4d8b2a3
Jax <jaxxonhalsall@gmail.com>
2026-09-11 13:20:11 +0930

    Added all conversion options and made converter functional

--- UniversalCalculator/UnitsConverter.xaml.cs
+++ UniversalCalculator/UnitsConverter.xaml.cs
@@ -23,6 +23,16 @@ namespace Calculator
 	/// </summary>
 	public sealed partial class UnitsConverter : Page
 	{
+
+		private readonly Func<double, double>[] _converters =
+		{
+			c => (c * 9 / 5) + 32 ,
+			f => (f - 32) * 5 / 9 ,
+			k => k * 2.20462,
+			p => p / 2.20462,
+			m => m * 3.28084,
+			ft => ft / 3.28084,
+		};
 		public UnitsConverter()
 		{
 			InitializeComponent();
@@ -31,6 +41,31 @@ namespace Calculator
 		private void returnToMenuButton_Click(object sender, RoutedEventArgs e)
 		{
 			this.Frame.Navigate(typeof(MainMenu));
+		}
+
+		private void CalculateOptions()
+		{
+			try
+			{
+				Func<double, double> conversionFunc = _converters[conversionTypeComboBox.SelectedIndex];
+				double input = double.Parse(inputOneTextBox.Text);
+
+				outputTextBox.Text = conversionFunc(input).ToString();
+			}
+			catch
+			{
+				outputTextBox.Text = "NAN";
+			}
+		}
+
+		private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			CalculateOptions();
+		}
+
+		private void conversionTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			CalculateOptions();
 		}
 	}
 }
