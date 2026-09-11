079b2fb3bd6405dd49995b6b46c6ec117db6219b
Jax <jaxxonhalsall@gmail.com>
2026-09-11 12:08:03 +0930

    Added more UI to the UnitsConverter page

---
+++ UniversalCalculator/UnitsConverter.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class UnitsConverter : Page
+	{
+		public UnitsConverter()
+		{
+			InitializeComponent();
+		}
+	}
+}
